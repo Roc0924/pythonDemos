b6d9c26a093ee4be92042c8eb2bb7fc97c02a07e
wangzhenpeng <wangzhenpeng@gomeplus.com>
2017-04-18 17:44:58 +0800

    first commit

---
+++ 2017-4-18115143/TestIdController.py
@@ -0,0 +1,9 @@
+# -*- encoding=utf8 -*-
+# Create with JetBrains PyCharm
+# Author               : wangzhenpeng
+# Date                 : 2017/4/18
+# Time                 : 11:54
+# Description          : 
+#
+
+import io
